ba0dfccd5cc0dd01c0e1f5245ad52f30c39dded3
Krathee <kanikarathee015@gmail.com>
2020-05-05 12:26:27 -0500

    USer table is create

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_014220) do
+ActiveRecord::Schema.define(version: 2020_05_05_171230) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
