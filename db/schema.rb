54a41f4815dcf11b119efd74f60662b24faa8543
Krathee <kanikarathee015@gmail.com>
2020-05-05 14:36:05 -0500

    Added user_id and updated all article with user_id as 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_171230) do
+ActiveRecord::Schema.define(version: 2020_05_05_191850) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
