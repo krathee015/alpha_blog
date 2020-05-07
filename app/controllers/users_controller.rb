9439f07b7f19e5ba8d714a1f53a4b0b50d0b707d
Krathee <kanikarathee015@gmail.com>
2020-05-07 12:01:57 -0500

    done again show page, did users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @a_index = @user.articles
+    end
+
     def new
         @user = User.new
     end
+
+    def index
+        @users = User.all 
+    end 
 
     def edit
         @user = User.find(params[:id])
