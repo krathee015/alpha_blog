a736369b8b9bc24bb3fb5a5e5ff27b74ce30b26e
Krathee <kanikarathee015@gmail.com>
2020-05-08 14:40:07 -0500

    user edit and update url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update ]
-   
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
+
+
     def show
         
         @a_index = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -53,6 +56,13 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] ="you can only edit your own profile"
+            redirect_to @user
+        end
     end
 
 end
