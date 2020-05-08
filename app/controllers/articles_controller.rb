6661a81d24306283d76d076a45536a10ba60db9d
Krathee <kanikarathee015@gmail.com>
2020-05-08 14:31:24 -0500

    login more restrictions regarding edit using url

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController<ApplicationController
     
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
         
@@ -10,7 +12,7 @@ class ArticlesController<ApplicationController
         @a_index = Article.paginate(page: params[:page], per_page: 5)
     end
      
-    def new
+    def new 
         @article = Article.new 
     end 
 
@@ -58,5 +60,12 @@ class ArticlesController<ApplicationController
     def article_pramas
         params.require(:article).permit(:title, :description)
     end 
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] ="you can only edit or delete your own article"
+            redirect_to @article
+        end
+    end
 
 end
